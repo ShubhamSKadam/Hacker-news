99b4c1d783d0044f999ec9fc19ea41d091d67e81
ShubhamSKadam <shubhamskadam99@outlook.com>
2023-06-21 11:11:39 +0530

    React props

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,55 @@
 import React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function Search() {
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    // value of the target
+    console.log(event.target.value);
+  };
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input type="text" id="search" />
+      <input type="text" id="search" onChange={handleChange} />
     </div>
   );
-}
+};
 
-function List() {
-  return (
-    <>
-      {list.map((Element) => {
-        return (
-          <li key={Element.objectID}>
-            <span>
-              <a href={Element.url}>{Element.title} </a>
-            </span>
-            <span>Author : {Element.author}, </span>
-            <span>Comments : {Element.num_comments}, </span>
-            <span>Points : {Element.points}</span>
-          </li>
-        );
-      })}
-    </>
-  );
-}
+const List = () => (
+  <div>
+    {list.map((Element) => {
+      return (
+        <li key={Element.objectID}>
+          <span>
+            <a href={Element.url}>{Element.title} </a>
+          </span>
+          <span>Author : {Element.author}, </span>
+          <span>Comments : {Element.num_comments}, </span>
+          <span>Points : {Element.points}</span>
+        </li>
+      );
+    })}
+  </div>
+);
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -56,6 +58,5 @@ function App() {
       <List />
     </div>
   );
-}
-
+};
 export default App;
